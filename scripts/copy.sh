a75c1967cf47dc3cad1e41450c28ddf6d58a7bda
everton17 <everton270694@gmail.com>
2018-11-11 14:13:09 -0200

    Update copy.sh

--- scripts/copy.sh
+++ scripts/copy.sh
@@ -1,1 +1,24 @@
-touch /var/www/html/arquivo123.html
+#!/bin/bash
+
+#Dando permissões nas pastas
+sudo chown -R apache:apache /var/www/html/
+sudo chmod -R 755 /var/www
+
+#Criando pastas do Apache
+sudo mkdir /etc/httpd/sites-available
+sudo mkdir /etc/httpd/sites-enabled
+
+#Criando VHOST e Criando Link Simbolico
+echo "
+<VirtualHost *:80>
+    ServerName www.testecodedeploy.com.br
+    ServerAlias testecodedeploy.com.br
+    DocumentRoot /var/www/site/
+</VirtualHost>" > /etc/httpd/sites-available/site.conf
+
+sudo ln -s /etc/httpd/sites-available/site.conf /etc/httpd/sites-enabled/site.conf
+
+#Restartando Apache
+sudo systemctl restart httpd.service
+
+echo Prontinho
